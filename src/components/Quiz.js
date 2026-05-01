b2bec776e2909010efc60e78147282cf1802fd0d
ajgodee23 <ajgodee@gmail.com>
2026-04-30 23:42:11 -0400

    Update Quiz.js

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
-import { BiDrink } from 'react-icons/bi'; // A cocktail icon
+// import { BiDrink } from 'react-icons/bi'; // A cocktail icon
 
 
 const Quiz = ({ onComplete, loading }) => {
+
+
+
   // Store all answers in one state object
   const [answers, setAnswers] = useState({
     mediaType: 'book',
@@ -16,7 +19,7 @@ const Quiz = ({ onComplete, loading }) => {
     const { name, value } = e.target;
     // Convert string 'true'/'false' back to actual booleans for the API
     const finalValue = value === 'true' ? true : value === 'false' ? false : value;
-    
+
     setAnswers(prev => ({
       ...prev,
       [name]: finalValue
@@ -28,94 +31,100 @@ const Quiz = ({ onComplete, loading }) => {
     onComplete(answers);
   };
 
+  // Athena's HTML
   return (
     <div className="container">
+      <h1 className="quiz-header">Welcome to Midnight Menu!</h1>
+      <h4 className="quiz-subhead">Find your perfect weekend activity with our easy to take quiz! We will suggest a cocktail, movie, and book for
+        your perfect night.</h4>
+      <p className="intro">	&or;</p>
+
       <form className="quiz-container" onSubmit={handleSubmit}>
-      <h2>Customize Your Vibe</h2>
+        <h2 className="quiz-header">Customize Your Vibe</h2>
 
-      {/* Question 1: Media Type */}
-      <div className="question-block">
-        <p>1. Are you more of a:</p>
-        <label>
-          <input type="radio" name="mediaType" value="book" onChange={handleChange} /> Bookworm
-        </label>
-        <label>
-          <input type="radio" name="mediaType" value="movie" onChange={handleChange} /> Movie buff
-        </label>
-        <label>
-          <input type="radio" name="mediaType" value="either" onChange={handleChange} /> Either or!
-        </label>
-      </div>
+        {/* Question 1: Media Type */}
+        <div className="question-block, card2">
+          <p>1. Are you more of a:</p>
+          <label>
+            <input type="radio" name="mediaType" value="book" onChange={handleChange} /> Bookworm
+          </label>
+          <label>
+            <input type="radio" name="mediaType" value="movie" onChange={handleChange} /> Movie buff
+          </label>
+          <label>
+            <input type="radio" name="mediaType" value="either" onChange={handleChange} /> Either or!
+          </label>
+        </div>
 
-      {/* Question 2: Mature Content */}
-      <div className="question-block">
-        <p>2. Mature Stuff ok?</p>
-        <label>
-          <input type="radio" name="isMature" value="true" onChange={handleChange} /> Yes
-        </label>
-        <label>
-          <input type="radio" name="isMature" value="false" onChange={handleChange} /> No
-        </label>
-      </div>
+        {/* Question 2: Mature Content */}
+        <div className="question-block, card2">
+          <p>2. Mature Stuff ok?</p>
+          <label>
+            <input type="radio" name="isMature" value="true" onChange={handleChange} /> Yes
+          </label>
+          <label>
+            <input type="radio" name="isMature" value="false" onChange={handleChange} /> No
+          </label>
+        </div>
 
-      {/* Question 3: Genre */}
-      <div className="question-block">
-        <p>3. What kind of genre?</p>
-        <select name="genreKey" onChange={handleChange} value={answers.genreKey}>
-          <option value="murder_mystery">Murder mystery</option>
-          <option value="action_adventure">Action/Adventure</option>
-          <option value="romance">Romance</option>
-          <option value="non_fiction">Non-fiction/documentary</option>
-          <option value="fantasy_sci_fi">Fantasy/Sci-fi</option>
-          <option value="surprise">Surprise Me!</option>
-        </select>
-      </div>
+        {/* Question 3: Genre */}
+        <div className="question-block, card2">
+          <p>3. What kind of genre?</p>
+          <select name="genreKey" onChange={handleChange} value={answers.genreKey}>
+            <option value="murder_mystery">Murder mystery</option>
+            <option value="action_adventure">Action/Adventure</option>
+            <option value="romance">Romance</option>
+            <option value="non_fiction">Non-fiction/documentary</option>
+            <option value="fantasy_sci_fi">Fantasy/Sci-fi</option>
+            <option value="surprise">Surprise Me!</option>
+          </select>
+        </div>
 
-      {/* Question 4: Alcohol */}
-      <div className="question-block">
-        <p>4. Are we feeling like a:</p>
-        <label>
-          <input type="radio" name="isAlcoholic" value="true" onChange={handleChange} /> Alcoholic drink
-        </label>
-        <label>
-          <input type="radio" name="isAlcoholic" value="false" onChange={handleChange} /> Non-alcoholic drink
-        </label>
-      </div>
+        {/* Question 4: Alcohol */}
+        <div className="question-bloc, card2">
+          <p>4. Are we feeling like a:</p>
+          <label>
+            <input type="radio" name="isAlcoholic" value="true" onChange={handleChange} /> Alcoholic drink
+          </label>
+          <label>
+            <input type="radio" name="isAlcoholic" value="false" onChange={handleChange} /> Non-alcoholic drink
+          </label>
+        </div>
 
-      {/* Question 5: Flavor */}
-      <div className="question-block">
-        <p>5. What sort of flavor profile?</p>
-        <label>
-          <input type="radio" name="flavor" value="sweet_fruity" onChange={handleChange} /> Fruity
-        </label>
-        <label>
-          <input type="radio" name="flavor" value="chocolatey" onChange={handleChange} /> Chocolatey
-        </label>
-        <label>
-          <input type="radio" name="flavor" value="savory_smoky" onChange={handleChange} /> Savory/Smoky
-        </label>
-        <label>
-          <input type="radio" name="flavor" value="sour_bitter" onChange={handleChange} /> Sour/Bitter
-        </label>
-        <label>
-          <input type="radio" name="flavor" value="earthy_herby" onChange={handleChange} /> Earthy/Herby
-        </label>
-        <label>
-          <input type="radio" name="flavor" value="surprise" onChange={handleChange} /> Surprise Me!
-        </label>
-      </div>
+        {/* Question 5: Flavor */}
+        <div className="question-block, card2">
+          <p>5. What sort of flavor profile?</p>
+          <label>
+            <input type="radio" name="flavor" value="sweet_fruity" onChange={handleChange} /> Fruity
+          </label>
+          <label>
+            <input type="radio" name="flavor" value="chocolatey" onChange={handleChange} /> Chocolatey
+          </label>
+          <label>
+            <input type="radio" name="flavor" value="savory_smoky" onChange={handleChange} /> Savory/Smoky
+          </label>
+          <label>
+            <input type="radio" name="flavor" value="sour_bitter" onChange={handleChange} /> Sour/Bitter
+          </label>
+          <label>
+            <input type="radio" name="flavor" value="earthy_herby" onChange={handleChange} /> Earthy/Herby
+          </label>
+          <label>
+            <input type="radio" name="flavor" value="surprise" onChange={handleChange} /> Surprise Me!
+          </label>
+        </div>
 
-      <button type="submit" disabled={loading} className="submit-btn">
-        {loading ? "Mixing..." : "Get My Recommendation"}
-      </button>
-    </form>
+        <button type="submit" disabled={loading} className="submit-btn">
+          {loading ? "Mixing..." : "Get My Recommendation"}
+        </button>
+      </form>
 
-      <button 
-        onClick={handleSubmit} 
+      <button
+        onClick={handleSubmit}
         disabled={loading} // Prevent double clicks
         className="submit-btn"
       >
-        {loading ? <BiDrink className="spinning-icon" /> : "Generate"}
+        {/* {loading ? <BiDrink className="spinning-icon" /> : "Generate"} */}
       </button>
     </div>
   );
