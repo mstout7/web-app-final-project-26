c39b7b4008fe3a90994df0dfd3142da43c896d7e
ajgodee23 <ajgodee@gmail.com>
2026-05-05 19:31:03 -0400

    Update Quiz.js

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -27,6 +27,56 @@ const Quiz = ({ onComplete, loading }) => {
     onComplete(answers);
   };
 
+  // Question to question transition
+    const q1Ref = useState(null);
+    const q2Ref = useState(null);
+    const q3Ref = useState(null);
+    const q4Ref = useState(null);
+    const q5Ref = useState(null);
+  
+    const scrollToQ1 = () => {
+      q1Ref.current.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
+    };
+    const handleAnswerQ1 = (answer) => {
+  
+      q2Ref.current.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
+    }
+    const handleAnswerQ2 = (answer) => {
+  
+      q3Ref.current.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
+  
+    };
+    const handleAnswerQ3 = (answer) => {
+  
+      q4Ref.current.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
+  
+      const { name, value } = answer.target;
+      const finalValue = value === 'true' ? true : value === 'false' ? false : value;
+  
+      setAnswers(prev => ({
+        ...prev,
+        [name]: finalValue
+      }));
+    };
+    const handleAnswerQ4 = (answer) => {
+      q5Ref.current.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
+    };
+
   // Athena's HTML
   return (
     <div className="bg">
@@ -37,41 +87,42 @@ const Quiz = ({ onComplete, loading }) => {
           <h4 className="quiz-subhead">Find your perfect weekend activity with our easy to take quiz! <br/> <br/> We will suggest a cocktail, movie, and book for your perfect night.</h4>
           <img src="nbg goldstar.png" alt="Star" />
         </div>
-        <p className="intro">	&or;</p>
+        <p onClick={scrollToQ1} className='quizBtn'>Start &nbsp; Quiz</p>
+        <button onClick={scrollToQ1} className="intro">	&or;</button>
       </div>
 
       <form className="quiz-container" onSubmit={handleSubmit}>
-        <h2 className="quiz-header2">Customize Your Vibe</h2>
+        <h2 ref={q1Ref} className="quiz-header2">Customize Your Vibe</h2>
 
         {/* Question 1: Media Type */}
         <div className="question-block, card2">
           <p>1. Are you more of a:</p>
           <label>
-            <input type="radio" name="mediaType" value="book" onChange={handleChange} /> Bookworm
+            <input type="radio" name="mediaType" value="book" onChange={handleAnswerQ1} /> Bookworm
           </label>
           <label>
-            <input type="radio" name="mediaType" value="movie" onChange={handleChange} /> Movie buff
+            <input type="radio" name="mediaType" value="movie" onChange={handleAnswerQ1} /> Movie buff
           </label>
           <label>
-            <input type="radio" name="mediaType" value="either" onChange={handleChange} /> Either or!
+            <input type="radio" name="mediaType" value="either" onChange={handleAnswerQ1} /> Either or!
           </label>
         </div>
 
         {/* Question 2: Mature Content */}
-        <div className="question-block, card2">
+        <div ref={q2Ref} className="question-block, card2">
           <p>2. Mature Stuff ok?</p>
           <label>
-            <input type="radio" name="isMature" value="true" onChange={handleChange} /> Yes
+            <input type="radio" name="isMature" value="true" onChange={handleAnswerQ2} /> Yes
           </label>
           <label>
-            <input type="radio" name="isMature" value="false" onChange={handleChange} /> No
+            <input type="radio" name="isMature" value="false" onChange={handleAnswerQ2} /> No
           </label>
         </div>
 
         {/* Question 3: Genre */}
-        <div className="question-block, card2">
+        <div ref={q3Ref} className="question-block, card2">
           <p>3. What kind of genre?</p>
-          <select className="genre" name="genreKey" onChange={handleChange} value={answers.genreKey}>
+          <select className="genre" name="genreKey" onChange={handleAnswerQ3} value={answers.genreKey}>
             <option className="genre2" value="murder_mystery">Murder mystery</option>
             <option value="action_adventure">Action/Adventure</option>
             <option value="romance">Romance</option>
@@ -82,18 +133,18 @@ const Quiz = ({ onComplete, loading }) => {
         </div>
 
         {/* Question 4: Alcohol */}
-        <div className="question-block, card2">
+        <div ref={q4Ref} className="question-block, card2">
           <p>4. Are we feeling like a:</p>
           <label>
-            <input type="radio" name="isAlcoholic" value="true" onChange={handleChange} /> Alcoholic drink
+            <input type="radio" name="isAlcoholic" value="true" onChange={handleAnswerQ4} /> Alcoholic drink
           </label>
           <label>
-            <input type="radio" name="isAlcoholic" value="false" onChange={handleChange} /> Non-alcoholic drink
+            <input type="radio" name="isAlcoholic" value="false" onChange={handleAnswerQ4} /> Non-alcoholic drink
           </label>
         </div>
 
         {/* Question 5: Flavor */}
-        <div className="question-block, card2">
+        <div ref={q5Ref} className="question-block, card2">
           <p>5. What sort of flavor profile?</p>
           <label>
             <input type="radio" name="flavor" value="sweet_fruity" onChange={handleChange} /> Fruity
