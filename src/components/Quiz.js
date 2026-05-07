2d171687a294a6c02f88020c740030e9ca3d07ab
Elle Smith <gsmith89@kent.edu>
2026-05-06 23:31:30 -0400

    Final Upload (for me, anyway)

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -30,11 +30,11 @@ const Quiz = ({ onComplete, loading }) => {
 
   return (
     <div className="container">
+      <h1>Midnight Menu</h1>
       <form className="quiz-container" onSubmit={handleSubmit}>
-      <h2>Customize Your Vibe</h2>
 
       {/* Question 1: Media Type */}
-      <div className="question-block">
+      <div className="question-block" id="q1">
         <p>1. Are you more of a:</p>
         <label>
           <input type="radio" name="mediaType" value="book" onChange={handleChange} /> Bookworm
@@ -45,10 +45,13 @@ const Quiz = ({ onComplete, loading }) => {
         <label>
           <input type="radio" name="mediaType" value="either" onChange={handleChange} /> Either or!
         </label>
+        <div className='next' >
+          <a href="#q2">Next</a>
+        </div>
       </div>
 
       {/* Question 2: Mature Content */}
-      <div className="question-block">
+      <div className="question-block" id="q2">
         <p>2. Mature Stuff ok?</p>
         <label>
           <input type="radio" name="isMature" value="true" onChange={handleChange} /> Yes
@@ -56,12 +59,16 @@ const Quiz = ({ onComplete, loading }) => {
         <label>
           <input type="radio" name="isMature" value="false" onChange={handleChange} /> No
         </label>
+        <div className='next' >
+          <a href="#q1">Back</a>
+          <a href="#q3">Next</a>
+        </div>
       </div>
 
       {/* Question 3: Genre */}
-      <div className="question-block">
+      <div className="question-block" id="q3">
         <p>3. What kind of genre?</p>
-        <select name="genreKey" onChange={handleChange} value={answers.genreKey}>
+        <select className="dropdown" name="genreKey" onChange={handleChange} value={answers.genreKey}>
           <option value="murder_mystery">Murder mystery</option>
           <option value="action_adventure">Action/Adventure</option>
           <option value="romance">Romance</option>
@@ -69,10 +76,14 @@ const Quiz = ({ onComplete, loading }) => {
           <option value="fantasy_sci_fi">Fantasy/Sci-fi</option>
           <option value="surprise">Surprise Me!</option>
         </select>
+        <div className='next' >
+          <a href="#q2">Back</a>
+          <a href="#q4">Next</a>
+        </div>
       </div>
 
       {/* Question 4: Alcohol */}
-      <div className="question-block">
+      <div className="question-block" id="q4">
         <p>4. Are we feeling like a:</p>
         <label>
           <input type="radio" name="isAlcoholic" value="true" onChange={handleChange} /> Alcoholic drink
@@ -80,10 +91,14 @@ const Quiz = ({ onComplete, loading }) => {
         <label>
           <input type="radio" name="isAlcoholic" value="false" onChange={handleChange} /> Non-alcoholic drink
         </label>
+        <div className='next' >
+          <a href="#q3">Back</a>
+          <a href="#q5">Next</a>
+        </div>
       </div>
 
       {/* Question 5: Flavor */}
-      <div className="question-block">
+      <div className="question-block" id="q5">
         <p>5. What sort of flavor profile?</p>
         <label>
           <input type="radio" name="flavor" value="sweet_fruity" onChange={handleChange} /> Fruity
@@ -103,11 +118,14 @@ const Quiz = ({ onComplete, loading }) => {
         <label>
           <input type="radio" name="flavor" value="surprise" onChange={handleChange} /> Surprise Me!
         </label>
+        <div className='next' >
+          <a href="#q4">Back</a>
+        </div>
       </div>
 
-      <button type="submit" disabled={loading} className="submit-btn">
+      {/* <button type="submit" disabled={loading} className="submit-btn">
         {loading ? "Mixing..." : "Get My Recommendation"}
-      </button>
+      </button> */}
     </form>
 
       <button 
@@ -115,7 +133,7 @@ const Quiz = ({ onComplete, loading }) => {
         disabled={loading} // Prevent double clicks
         className="submit-btn"
       >
-        {loading ? <BiDrink className="spinning-icon" /> : "Generate"}
+        {loading ? <BiDrink className="spinning-icon" /> : "Generate My Night-In"}
       </button>
     </div>
   );
--- src/components/ResultCard.js
+++ src/components/ResultCard.js
@@ -16,6 +16,7 @@ const ResultCard = ({ data, onReset }) => {
 
   return (
     <div className="results-wrapper">
+      <h1>Midnight Menu</h1>
       <div className="results-container">
         
         {/* LEFT SIDE: THE MEDIA (Book or Movie) */}
@@ -87,15 +88,14 @@ const ResultCard = ({ data, onReset }) => {
             alt="Data provided by The New York Times" 
             className="nyt-logo"
           />
-        </div>
-        <div className="attribution-item">
           <img 
             src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg" 
             alt="Powered by TMDB" 
             className="tmdb-logo"
           />
-          <p>This product uses the TMDB and NYT APIs but is not endorsed or certified by either.</p>
         </div>
+        <p>This product uses the TMDB and NYT APIs but is not endorsed or certified by either.</p>
+
       </footer>
     </div>
 
